f958cd36fa70574339970b44b4cd327e161164a7
Joshua Higgins <joshuafhiggins@gmail.com>
2024-06-20 21:08:34 -0400

    Message type

--- server/src/types.rs
+++ server/src/types.rs
@@ -2,3 +2,19 @@
 pub trait RealmChat {
 	async fn test(name: String) -> String;
 }
+
+#[derive(Debug)]
+pub struct Message {
+	guid: String,
+	text: Option<String>,
+	attachments: Option<Vec<Attachment>>,
+	reply_to_guid: Option<String>,
+	reaction_emoji: Option<String>,
+	redact: bool,
+}
+
+#[derive(Debug)]
+pub struct Attachment {
+	guid: String,
+	//TODO
+}
